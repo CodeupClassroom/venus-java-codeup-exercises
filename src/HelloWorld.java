e3a40fcef7414e27d65681f27d02f2a4e160824a
kennethhowell <howell.kenneth.a@gmail.com>
2022-08-01 13:53:02 -0500

    finished exercise review

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String [] args){
+        System.out.println("Hello, World from the Venus cohort!");
+        System.out.println();
+
+        System.out.print("Hello ");
+        System.out.print("from ");
+        System.out.print("the ");
+        System.out.print("Venus ");
+        System.out.print("Cohort ");
+        System.out.print("to the ");
+        System.out.print("whole world :) ");
+    }
+}
